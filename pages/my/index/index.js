4e7c5e615b27820c72628626e6843a9ffe2486bf
yen <yening@mingyuanyun.com>
2017-06-27 19:35:11 +0800

    我的页面

--- pages/my/index/index.js
+++ pages/my/index/index.js
@@ -1,6 +1,35 @@
+var app = getApp();
+
 Page({
+  data:{
+    userInfo: {},
+  },
+  // 跳转到我的分享页面
+  gotoMyShare: function (e) {
+    wx.navigateTo({
+      url: '../my-share/index'
+    })
+  },
+ 
+  // 跳转到我的体验页面
+  gotoMyTaste: function () {
+    wx.navigateTo({
+      url: '../my-taste/index'
+    })
+  },
+  tapName: function (event) {
+    console.log(event)
+  },
+
   onLoad: function () {
     console.log('onLoad')
-    
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   }
 })
