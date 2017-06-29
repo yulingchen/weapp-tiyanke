3e5f0b9e58696180da2b77470130e6e65b0bcdb8
yen <yen@mingyuanyun.com>
2017-06-29 18:30:56 +0800

    增加分享详情，增加发布分享，删除一些图片，增加wxparse和weui库

--- pages/share/index/index.js
+++ pages/share/index/index.js
@@ -3,4 +3,15 @@ Page({
     console.log('onLoad')
     
   },
+  gotoDetails(e){
+    let data = e.target.dataset
+    wx.navigateTo({
+      url: '../details/index',
+    })
+  },
+  publishShare(){
+    wx.navigateTo({
+      url: '../add/index',
+    })
+  },
 })
