571708a6232ffe6f65cde92d9b2710b4988eb04d
yulingchen <yulingchen@vip.qq.com>
2017-07-01 21:18:15 +0800

    update

--- pages/my/my-share/index.js
+++ pages/my/my-share/index.js
@@ -1,6 +1,37 @@
+var app = getApp();
+var util = require('../../../utils/util.js')
+
 Page({
+	data:{
+    reports:[]
+  },
   onLoad: function () {
-    console.log('onLoad')
-    
+    var self=this;
+
+    (function shared(){
+      wx.request({
+        url: 'https://m.tiyanke.com/experience/project/refReports',
+        method: 'POST',
+        data: {
+          wxappSessionId: wx.getStorageSync('wxappSessionId')
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function(res) {
+          if(res.statusCode==401){
+            app.login(shared)
+            return;
+          }
+
+          self.setData({
+	          reports: res.data.data.map(function(report){
+	            report.report_time = util.formatTime(new Date(report.report_time))
+	            return report
+	          })
+	        })
+        }
+      })
+    })()
   }
 })
