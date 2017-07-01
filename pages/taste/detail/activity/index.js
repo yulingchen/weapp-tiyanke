846878b15d21ddf4bd8ddab924207b768c628fd0
玉龄陈 <yulingchen@yulingchendeMac-mini.local>
2017-07-01 13:45:07 +0800

    update

--- pages/taste/detail/activity/index.js
+++ pages/taste/detail/activity/index.js
@@ -5,35 +5,47 @@ var app = getApp();
 Page({
 	data:{
 		project:{},
-    users:[]
+    users:[],
+    willStatus:''
 	},
   onLoad: function (option) {
   	var self = this;
-  	wx.request({
-		  url: 'https://m.tiyanke.com/experience/project/detail', 
-		  data:{
-		  	projectId: option.projectId
-		  },
-		  success: function(res) {
-		  	var project = res.data.data.project
-        var users = res.data.data.users
 
-        project.startDateTime = util.formatTime(new Date(project.startDateTime))
-        project.endDateTime = util.formatTime(new Date(project.endDateTime))
-		  	wx.setNavigationBarTitle({
-				  title: project && project.title
-				})
+    (function getDetail(){
+    	wx.request({
+  		  url: 'https://m.tiyanke.com/experience/project/detail', 
+        method: 'POST',
+  		  data:{
+  		  	projectId: option.projectId,
+          wxappSessionId: wx.getStorageSync('wxappSessionId')
+  		  },
+  		  success: function(res) {
+          if(res.statusCode==401){
+            app.login(getDetail)
+            return;
+          }
 
-				WxParse.wxParse('detail', 'html', project.content, self, 5);
-		    self.setData({
-		    	project: project,
-          users: users
-		    })
-		  }
-		})
+  		  	var project = res.data.data.project
+          project.startDateTime = util.formatTime(new Date(project.startDateTime))
+          project.endDateTime = util.formatTime(new Date(project.endDateTime))
+  		  	wx.setNavigationBarTitle({
+  				  title: project && project.title
+  				})
+
+  				WxParse.wxParse('detail', 'html', project.content, self, 5);
+  		    self.setData({
+  		    	project: project,
+            users: res.data.data.users,
+            willStatus: res.data.data.willStatus
+  		    })
+  		  }
+  		})
+    })()
   },
 
+  // 报名
   enrollExp: function(e){
+    var self = this
   	var projectId = e.currentTarget.dataset.projectid;
 
   	(function enroll(){
@@ -53,13 +65,27 @@ Page({
             return;
           }
 
-          if(res.result){
+          if(res.data.result){
           	wx.showToast({title: '报名成功'})
+
+            var flow = self.data.project.flow
+            var index = flow.indexOf('报名')
+            self.setData({
+              willStatus: flow[index+1]
+            })
           }else{
           	wx.showToast({title: res.data.msg})
           }
         }
       })
     })()
+  },
+
+  // 分享
+  shareExp: function(e){
+    var projectId = e.currentTarget.dataset.projectid;
+    wx.navigateTo({
+      url: '../../../share/add/index?projectId='+projectId
+    })
   }
 })
